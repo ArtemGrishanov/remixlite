f370c07fb9b15de8e082b04169bba6a5ddf66d57
Max <microbking@gmail.com>
2021-01-20 17:03:19 +0700

    blocksEnum (#ENGA-1275)

---
+++ src/typeFiles/js/blocks/blocksEnum.js
@@ -0,0 +1,14 @@
+const BLOCK = {
+    text: 1,
+    image: 2,
+    embedInteractyProject: 3,
+    flipCards: 4,
+    youtubeVideo: 5,
+    button: 6,
+    interactiveImage: 7,
+    hiddenObjects: 8,
+    quiz: 9,
+}
+Object.freeze(BLOCK)
+
+export default BLOCK
