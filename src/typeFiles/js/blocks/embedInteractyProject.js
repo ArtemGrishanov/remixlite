54f3ced34aa7e837ac281b262672a09bc172ff83
timuravdeev <timur.avdeev@quantumsoft.ru>
2021-02-02 19:11:18 +0700

    ENGA-1227: modified the Embed Code block to use the src path from the data

--- src/typeFiles/js/blocks/embedInteractyProject.js
+++ src/typeFiles/js/blocks/embedInteractyProject.js
@@ -5,8 +5,16 @@ export default function(cnt, { methods }) {
     return {
         render: data => {
             methods.add(cnt, methods.parse(template, data), data.t)
+            let src = "//interacty.me/legacy/editor_assets/js/l.js";
+            const { embedCode } = data;
+            const scrIndex = embedCode.indexOf('<script')
+            const srcStartIndex = embedCode.indexOf('src="', scrIndex + 1)
+            const srcEndIndex = embedCode.indexOf('.js"', srcStartIndex + 1);
+            if (~scrIndex && ~srcStartIndex && ~srcEndIndex) {
+                src = embedCode.slice(srcStartIndex+5, srcEndIndex+3);
+            }
             const script  = document.createElement("script");
-            script.src = "//interacty.me/legacy/editor_assets/js/l.js"
+            script.src = src;
             cnt.appendChild(script);
         }
     }
