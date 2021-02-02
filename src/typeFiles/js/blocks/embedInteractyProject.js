8e8de7febc38bbe3a1efeb90b14184fa8ea4f653
timuravdeev <timur.avdeev@quantumsoft.ru>
2021-02-02 21:47:18 +0700

    ENGA-1227: changes appropriate to the PR comments

--- src/typeFiles/js/blocks/embedInteractyProject.js
+++ src/typeFiles/js/blocks/embedInteractyProject.js
@@ -6,12 +6,11 @@ export default function(cnt, { methods }) {
         render: data => {
             methods.add(cnt, methods.parse(template, data), data.t)
             let src = "//interacty.me/legacy/editor_assets/js/l.js";
-            const { embedCode } = data;
-            const scrIndex = embedCode.indexOf('<script')
-            const srcStartIndex = embedCode.indexOf('src="', scrIndex + 1)
-            const srcEndIndex = embedCode.indexOf('.js"', srcStartIndex + 1);
-            if (~scrIndex && ~srcStartIndex && ~srcEndIndex) {
-                src = embedCode.slice(srcStartIndex+5, srcEndIndex+3);
+            try {
+                const { embedCode } = data;
+                src = /<script.*?src=['"](.*?)['"]/.exec(embedCode)[1];
+            } catch (err) {
+                console.error(`[RemixLite | Block type: 10 (ThenNow), ID: ${data.id}]`, err);
             }
             const script  = document.createElement("script");
             script.src = src;
