425f3015e096de5f57cc7cab155059cd03637a1a
proaleks94 <aleksey.pronin@quantumsoft.ru>
2021-02-06 14:23:59 +0700

    update

--- src/loader/session.js
+++ src/loader/session.js
@@ -1,7 +1,10 @@
 import { API_URL } from './constants'
+import { httpRequest } from './utils'
 
 export default class SESSION {
     #createdSession = null
+
+    #isAwaiting = false
 
     #clientId
     #projectId
@@ -26,36 +29,43 @@ export default class SESSION {
     // [PUBLIC] Send activity to server
     sendActivity = async () => {
         try {
-            if (!this.#createdSession) {
-                const response = await fetch(`${API_URL}/api/sessions`, {
-                    method: 'POST',
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Client-Key': this.#clientId
-                    },
-                    body: JSON.stringify({
-                        id: null,
-                        projectId: this.#projectId,
-                        utmCampaign: this.#utmCampaign ? this.#utmCampaign.slice(0, 128) : null,
-                        utmSource: this.#utmSource ? this.#utmSource.slice(0, 128) : null,
-                        utmMedium: this.#utmMedium ? this.#utmMedium.slice(0, 128) : null,
-                        utmContent: this.#utmContent ? this.#utmContent.slice(0, 128) : null,
-                        referenceTail: this.#referenceTail ? this.#referenceTail.slice(1, 513) : null,
-                        sourceReference: this.#sourceReference ? this.#sourceReference.slice(0, 512) : null
-                    })
-                });
-                this.#createdSession = await response.json();
-            } else {
-                await fetch(`${___API_URL___}/api/sessions/${this.#createdSession.id}/refresh`, {
-                    method: 'PATCH',
-                    headers: {
-                        'Client-Key': this.#clientId
-                    }
-                });
+            if (!this.#isAwaiting) {
+                this.#isAwaiting = true
+                if (!this.#createdSession) {
+                    const response = await httpRequest(`${API_URL}/api/sessions`, {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            'Client-Key': this.#clientId
+                        },
+                        body: JSON.stringify({
+                            id: null,
+                            projectId: this.#projectId,
+                            utmCampaign: this.#utmCampaign ? this.#utmCampaign.slice(0, 128) : null,
+                            utmSource: this.#utmSource ? this.#utmSource.slice(0, 128) : null,
+                            utmMedium: this.#utmMedium ? this.#utmMedium.slice(0, 128) : null,
+                            utmContent: this.#utmContent ? this.#utmContent.slice(0, 128) : null,
+                            referenceTail: this.#referenceTail ? this.#referenceTail.slice(1, 513) : null,
+                            sourceReference: this.#sourceReference ? this.#sourceReference.slice(0, 512) : null
+                        }),
+                        timeout: 10000
+                    });
+                    this.#createdSession = await response.json();
+                } else {
+                    await httpRequest(`${API_URL}/api/sessions/${this.#createdSession.id}/refresh`, {
+                        method: 'PATCH',
+                        headers: {
+                            'Client-Key': this.#clientId
+                        },
+                        timeout: 7000
+                    });
+                }
             }
         } catch (err) {
             console.error(err);
+        } finally {
+            this.#isAwaiting = false
         }
     }
 }
