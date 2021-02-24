f86841e101961788a3de16008e3c8cc310d01b84
Cleric57 <Cleric57@gmail.com>
2021-02-24 15:07:06 +0700

    ENGA-1227 Embed Code bug fix (#5)
    * ENGA-1227: modified the Embed Code block to use the src path from the data
    
    * ENGA-1227: changes appropriate to the PR comments
    
    * ENGA-1227: minor changes
    
    Co-authored-by: timuravdeev <timur.avdeev@quantumsoft.ru>

--- src/typeFiles/js/blocks/embedInteractyProject.js
+++ src/typeFiles/js/blocks/embedInteractyProject.js
@@ -1,3 +1,5 @@
+import log from "../utils/log";
+
 // Template
 const template = `<div>{{embedCode}}</div>`
 
@@ -5,8 +7,15 @@ export default function(cnt, { methods }) {
     return {
         render: data => {
             methods.add(cnt, methods.parse(template, data), data.t)
+            let src = "//interacty.me/legacy/editor_assets/js/l.js";
+            try {
+                const { embedCode } = data;
+                src = /<script.*?src=['"](.*?)['"]/.exec(embedCode)[1];
+            } catch (err) {
+                log('error', '3 (EmbedInteractyProject)', data.id, null, err)
+            }
             const script  = document.createElement("script");
-            script.src = "//interacty.me/legacy/editor_assets/js/l.js"
+            script.src = src;
             cnt.appendChild(script);
         }
     }
